f0810e9b92150ccd4bbf9eef29d96e7c6b5b1ede
ikyrie <mah.c5r@gmail.com>
2025-01-24 11:54:27 -0300

    Pedindo permissão, pegando a localização e mandando resposta caso esteja sem permissão

--- lib/map_page.dart
+++ lib/map_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
@@ -23,6 +25,32 @@ class _MapPage extends State<MapPage> {
           myLocationEnabled: true,
           initialCameraPosition: cameraPosition,
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.location_on),
+        onPressed: () async {
+          await Permission.location.request();
+          bool hasPermission = await Permission.location.isGranted;
+          if (hasPermission) {
+            Position position = await Geolocator.getCurrentPosition();
+            print("${position.latitude}  ${position.longitude}");
+          } else {
+            showDialog(
+              context: context,
+              builder: (context) => AlertDialog(
+                title: const Text("Permissão negada"),
+                content: Text("Por favor, permita a permissão de localização"),
+                actions: [
+                  TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: Text("OK"))
+                ],
+              ),
+            );
+          }
+        },
       ),
     );
   }
